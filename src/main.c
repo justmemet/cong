91651dc75b9e1646c4bf6b2a2abde39dc8a3f1a8
memet <93953223+justmemet@users.noreply.github.com>
2023-08-13 20:54:24 -0300

    make the code readable

--- src/main.c
+++ src/main.c
@@ -2,94 +2,100 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <unistd.h>
 
-#define drawball(w, b)      drawobj(w, (b).y, (b).x, (b).ch);
-#define drawpaddle(w, p)    drawobj(w, (p).y, (p).x, (p).ch);
+#define draw_ball(window, ball)   draw_obj(window, (ball).y, (ball).x, (ball).character);
+#define draw_paddle(window, p) draw_obj(window, (p).y, (p).x, (p).character);
+
+#define create_player(window)  create_paddle(window, (getmaxy(window) - 1) / 2, getbegx(window) + 5);
+#define create_enemy(window)   create_paddle(window, (getmaxy(window) - 1) / 2, getmaxx(window) - 5);
 
 typedef struct Paddle {
-    char ch;
+    char character;
     int y, x;
 } PADDLE;
 
 typedef struct Ball {
-    char ch;
-    int y, x, yvel, xvel;
+    char character;
+    int y, x, y_velocity, x_velocity;
 } BALL;
 
-PADDLE createplr(WINDOW *w) {
+PADDLE create_paddle(WINDOW *window, int y, int x) {
     PADDLE p;
-    p.ch = '|';
-    p.y = (getmaxy(w) - 1) / 2;
-    p.x = getbegx(w) + 5;
+    p.character = '|';
+    p.y = y;
+    p.x = x;
     return p;
 }
 
-BALL createball(WINDOW *w) {
-    BALL b;
-    b.ch = 'O';
-    b.y = (getmaxy(w) - 1) / 2;
-    b.x = (getmaxx(w) - 1) / 2;
-    b.yvel = b.xvel = 1;
-    return b;
+BALL create_ball(WINDOW *window) {
+    BALL ball;
+    ball.character = 'O';
+    ball.y = (getmaxy(window) - 1) / 2;
+    ball.x = (getmaxx(window) - 1) / 2;
+    ball.y_velocity = ball.x_velocity = 1;
+    return ball;
 }
 
-void drawobj(WINDOW *w, int y, int x, char ch) {
-    mvwaddch(w, y, x, ch);
-    wrefresh(w);
+void draw_obj(WINDOW *window, int y, int x, char character) {
+    mvwaddch(window, y, x, character);
+    wrefresh(window);
 }
 
-void ballmov(WINDOW *w, BALL *b) {
-    mvwaddch(w, b->y, b->x, ' ');
-    if(b->y + 2 == getmaxy(w) || b->y - 1 == getbegy(w)) b->yvel = -b->yvel;
-    if(b->x + 2 == getmaxx(w) || b->x - 1 == getbegx(w)) b->xvel = -b->xvel; 
-    b->y += b->yvel;
-    b->x += b->xvel;
-    drawball(w, *b);
+void ball_move(WINDOW *window, BALL *ball) {
+    mvwaddch(window, ball->y, ball->x, ' ');
+    if(ball->y + 2 == getmaxy(window) || ball->y - 1 == getbegy(window)) ball->y_velocity = -ball->y_velocity;
+    if(ball->x + 2 == getmaxx(window) || ball->x - 1 == getbegx(window)) ball->x_velocity = -ball->x_velocity; 
+    ball->y += ball->y_velocity;
+    ball->x += ball->x_velocity;
+    draw_ball(window, *ball);
 }
 
-void plrmov(WINDOW *w, PADDLE *plr, chtype k) {
-    mvwaddch(w, plr->y, plr->x, ' ');
-    switch(k) {
-        case 119:
-            if(plr->y - 1 != getbegy(w)) plr->y--;
+void player_move(WINDOW *window, PADDLE *player, chtype key) {
+    mvwaddch(window, player->y, player->x, ' ');
+    const chtype UP = 119, DOWN = 115;
+    switch(key) {
+        case UP:
+            if(player->y - 1 != getbegy(window)) player->y--;
             break;
-        case 115:
-            if(plr->y + 2 != getmaxy(w)) plr->y++;
+        case DOWN:
+            if(player->y + 2 != getmaxy(window)) player->y++;
             break;
     }
-    drawpaddle(w, *plr);
+    draw_paddle(window, *player);
 }
 
-void startnc() {
+void start_curses() {
     initscr();
     cbreak();
     noecho();
     curs_set(0);
 }
 
-void initwin(WINDOW *w) {
-    nodelay(w, true);
-    box(w, 0, 0);
-    wmove(w, 1, 2);
-    wrefresh(w);
+void init_window(WINDOW *window) {
+    nodelay(window, true);
+    box(window, 0, 0);
+    wmove(window, 1, 2);
+    wrefresh(window);
 }
 
 int main() {
-    startnc();
-    WINDOW *curwin = stdscr;
-    initwin(curwin);
+    start_curses();
+    WINDOW *window = stdscr;
+    init_window(window);
 
-    BALL b = createball(curwin);
-    drawball(curwin, b);
+    BALL ball = create_ball(window);
+    draw_ball(window, ball);
 
-    PADDLE plr = createplr(curwin);
-    drawpaddle(curwin, plr);
+    PADDLE player = create_player(window);
+    draw_paddle(window, player);
+
+    PADDLE enemy = create_enemy(window);
+    draw_paddle(window, enemy);
 
     while(true) {
-        chtype k = wgetch(curwin);
-        plrmov(curwin, &plr, k);
-        ballmov(curwin, &b);
+        chtype key = wgetch(window);
+        player_move(window, &player, key);
+        ball_move(window, &ball);
         napms(40);
     }
 
